89ad9c1085ab1cefb8dd0f8e5e127f636384d8d5
Marek Bojec <marek.bojec@gmail.com>
2019-08-13 07:46:43 +0200

    Creating and styling header intro btn section

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import HeaderTopMenu from "./HeaderTopMenu";
+import HomeHeaderIntro from "./HomeHeaderIntro";
 import Image from '../assets/images/Home-Hero-Image.jpg'
 
 class HomeHeader extends Component{
@@ -8,13 +9,13 @@ class HomeHeader extends Component{
     return(
       <div className={'header-container'} style={{backgroundImage: `url(${Image})`}}>
         <div className={'row'} style={{height: '100%'}}>
-          <div className={'col-xs-6'}/>
-          <div className={'col-xs-6'}>
+          <div className={'col-xs-5 col-xxl-5'}/>
+          <div className={'col-xs-7 col-xxl-7'}>
             <div className={'row'} style={{height: '100%'}}>
               <div className={'col-xl-1 col-xxl-2'}/>
               <div className={'col-xs-12 col-xl-11 col-xxl-10'}><HeaderTopMenu/></div>
-              <div className={'col-xs-10'}/>
-              <div className={'col-xs-2'}></div>
+              <div className={'col-xs-11'}><HomeHeaderIntro/></div>
+              <div className={'col-xs-1'}/>
             </div>
           </div>
         </div>
