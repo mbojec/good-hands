f59641eb9308d04f9d51326cd8f0ed3c9ec1ce9a
Marek Bojec <marek.bojec@gmail.com>
2019-08-16 19:07:35 +0200

    Creating basic layout for foundation version of the list

--- src/components/HomeWhoWeHelp.js
+++ src/components/HomeWhoWeHelp.js
@@ -1,7 +1,58 @@
 import React, {Component} from "react";
 import Decoration from "../assets/svg/Decoration";
+import PaginationList from "./PaginationList";
+
+const list = [
+  {
+    title: `Fundacja "Dbam o Zdrowie"`,
+    subtitle: `Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.`,
+    info: `ubrania, jedzenie, sprzęt AGD, meble, zabawki`
+  },
+  {
+    title: `Fundacja  “Dla dzieci”`,
+    subtitle: `Cel i misja: Pomoc dzieciom z ubogich rodzin.`,
+    info: `ubrania, meble, zabawki`
+  },
+  {
+    title: `Fundacja  “Bez domu”`,
+    subtitle: `Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.`,
+    info: `ubrania, jedzenie, ciepłe koce`
+  },
+  {
+    title: `Fundacja "Dbam o Zdrowie2"`,
+    subtitle: `Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.`,
+    info: `ubrania, jedzenie, sprzęt AGD, meble, zabawki`
+  },
+  {
+    title: `Fundacja  “Dla dzieci2”`,
+    subtitle: `Cel i misja: Pomoc dzieciom z ubogich rodzin.`,
+    info: `ubrania, meble, zabawki`
+  },
+  {
+    title: `Fundacja  “Bez domu2”`,
+    subtitle: `Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.`,
+    info: `ubrania, jedzenie, ciepłe koce`
+  },
+  {
+    title: `Fundacja "Dbam o Zdrowie3"`,
+    subtitle: `Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.`,
+    info: `ubrania, jedzenie, sprzęt AGD, meble, zabawki`
+  },
+  {
+    title: `Fundacja  “Dla dzieci3”`,
+    subtitle: `Cel i misja: Pomoc dzieciom z ubogich rodzin.`,
+    info: `ubrania, meble, zabawki`
+  },
+  {
+    title: `Fundacja  “Bez domu3”`,
+    subtitle: `Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.`,
+    info: `ubrania, jedzenie, ciepłe koce`
+  },
+];
+
 
 class HomeWhoWeHelp extends Component{
+
   render() {
     return(
       <div className={'help-container'} >
@@ -27,6 +78,7 @@ class HomeWhoWeHelp extends Component{
           <div className={'col-xs-3'}/>
           <div className={'col-xs-1'}/>
           <div className={'col-xs-10'}>
+            <PaginationList list={list}/>
           </div>
           <div className={'col-xs-1'}/>
         </div>
