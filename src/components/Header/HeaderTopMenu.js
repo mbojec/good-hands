2251c9b1710bf4d7de045d0aa552347ad7c3aa98
Marek Bojec <marek.bojec@gmail.com>
2019-08-29 11:26:28 +0200

    Setting logout logic

--- src/components/Header/HeaderTopMenu.js
+++ src/components/Header/HeaderTopMenu.js
@@ -2,16 +2,23 @@ import React, {Component} from "react";
 import {Link} from 'react-router-dom';
 import { Link as ScrollLink} from 'react-scroll';
 import {withRedux} from "../../store/wrapper";
+import { withRouter } from 'react-router-dom';
 
 class HeaderTopMenu extends Component{
+
+  logout(){
+    this.props.onLogout();
+    this.props.history.push({pathname:'/wylogowano'})
+  }
+
 
   render() {
     return(
       <div className={'row'}>
         <div className={'col-xs-12 header__top-menu__login-register'}>
           {this.props.login && <div className={'header__top-menu__login-register__user-email'}><p>{`Cześć ${this.props.userEmail}`}</p></div>}
-          <button className={'header__top-menu__login-register__btn header__top-menu__login-register__btn--login'}><Link to={'/logowanie'}>Zaloguj</Link></button>
-          <button className={'header__top-menu__login-register__btn header__top-menu__login-register__btn--register header__top-menu__login-register__btn--pressed'}><Link to={'/rejestracja'}>Załóż konto</Link></button>
+          <button className={`header__top-menu__login-register__btn header__top-menu__login-register__btn--login ${this.props.login && 'header__top-menu__login-register__btn--pressed'}`}><Link to={'/logowanie'}>Zaloguj</Link></button>
+          <button className={`header__top-menu__login-register__btn header__top-menu__login-register__btn--login ${!this.props.login && 'header__top-menu__login-register__btn--pressed'}`}>{this.props.login? <a onClick={() => this.logout()}>Wyloguj</a>: <Link to={'/rejestracja'}>Załóż konto</Link> }</button>
         </div>
         <nav className={'col-xs-12 header__top-menu__navigation'}>
           <button className={'header__top-menu__navigation__btn header__top-menu__navigation__btn--pressed'}>Start</button>
@@ -33,5 +40,6 @@ class HeaderTopMenu extends Component{
   }
 }
 
-const connectedComponent = withRedux(HeaderTopMenu);
+const wrappedComponent = withRouter(HeaderTopMenu);
+const connectedComponent = withRedux(wrappedComponent);
 export {connectedComponent as HeaderTopMenu}
